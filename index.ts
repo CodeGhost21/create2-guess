b86f6c0a43696a0ac0c19359e0e5fa8f2dba97bd
Ghost Scripter <ghostscripter@zerolend.xyz>
2024-07-17 15:36:24 +0530

    added telegram bot

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 // import
+import 'dotenv/config'
 import { ethers } from "ethers";
+import TelegramBot from 'node-telegram-bot-api';
 import { getCreate2Address } from "./create2";
 
 // declare deployment parameters
@@ -15,12 +17,13 @@ const constructorArgs: any[] = [
   "0xe5159e75ba5f1C9E386A3ad2FC7eA75c14629572",
 ];
 
-console.log("constructor parameters", constructorTypes, constructorArgs);
+// console.log("constructor parameters", constructorTypes, constructorArgs);
 
-const job = () => {
+const job = async () => {
   let i = 0;
-
+  const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
   while (true) {
+
     const salt = ethers.id("" + i);
     // Calculate contract address
     const computedAddress = getCreate2Address({
@@ -31,17 +34,28 @@ const job = () => {
       constructorTypes: constructorTypes,
       constructorArgs: constructorArgs,
     });
-
     if (
       computedAddress.startsWith("0x0000") &&
       computedAddress.endsWith("0000")
     ) {
       console.log("found the right salt hash");
       console.log("salt", salt, computedAddress);
+      try {
+        const message = `
+🟢🟢🟢🟢
+
+found the right salt hash
+
+salt : ${salt}
+computedAddress: ${computedAddress}
+`;
+        await bot.sendMessage(process.env.TELEGRAM_CHAT_ID, message)
+        console.log('successful');
+      } catch (e) { console.log(e) };
       break;
     }
 
-    if (i % 100000 == 0) console.log(i, "salt", salt, computedAddress);
+    if (i % 100000 == 0) console.log(i, "salt", salt, computedAddress)
     i++;
   }
 };
